24b778111fc40d8a81f8a89c8b6983862cd1464e
skaimarvas <skaimarvas@gmail.com>
2024-01-18 12:44:02 +0300

    Bread class oluşturuldu.

---
+++ src/Bread.java
@@ -0,0 +1,11 @@
+public class Bread extends ProductForSale {
+    private int wheat;
+    public Bread(String type, int price, String description) {
+        super(type, price, description);
+    }
+
+    @Override
+    public void showDetails() {
+
+    }
+}
