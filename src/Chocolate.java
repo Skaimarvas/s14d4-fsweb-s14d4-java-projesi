dcbee838939d6fb5377d45ac4f27d865998c7982
skaimarvas <skaimarvas@gmail.com>
2024-01-18 12:40:21 +0300

    Coke classı oluşturuldu.

--- src/Chocolate.java
+++ src/Chocolate.java
@@ -1,4 +1,5 @@
 public class Chocolate extends ProductForSale{
+    private int cacaoSize;
     public Chocolate(String type, int price, String description) {
         super(type, price, description);
     }
---
+++ src/Coke.java
@@ -0,0 +1,11 @@
+public class Coke extends ProductForSale {
+    private int cafeinSize;
+    public Coke(String type, int price, String description) {
+        super(type, price, description);
+    }
+
+    @Override
+    public void showDetails() {
+
+    }
+}
